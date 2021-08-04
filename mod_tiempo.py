5484e766162b1ebf020d5c973c9570bbee00dc6b
Mercedes Pereyra <mercepereyra51@gmail.com>
2021-08-04 15:53:19 -0300

    agrego el modulo tiempo

---
+++ mod_tiempo.py
@@ -0,0 +1,17 @@
+import time
+
+
+def imprimir_segun_tiempo(tiempo: float, cadena, intervalo=.1):
+    tf = time.time() + tiempo
+    while time.time() < tf:
+        print(cadena)
+        time.sleep(intervalo)
+
+
+
+imprimir_algo = input(" Ingrese lo que quisiera ver: ")
+tiempo_a_imprimir = float(input(" ingrese un valor: "))
+
+t0 = time.time()
+prueba = imprimir_segun_tiempo(tiempo_a_imprimir, imprimir_algo)
+print(f"{time.time() - t0}")
